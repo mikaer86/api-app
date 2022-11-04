60fc2ec832c970677d6c1b9524ce5f8732e4dbc3
mikaer86 <mikaeleriksson123@gmail.com>
2022-11-04 15:10:13 +0100

    adjustments

--- js/index.js
+++ js/index.js
@@ -1,22 +1,24 @@
-async function renderWeatherElements(weather) {
+ function renderWeatherElements(weather) {
   console.log(weather);
   const location = document.getElementById('location');
   const temperature = document.getElementById('temperature');
   let paramLocation = document.createElement('p');
   let paramTemp = document.createElement('p');
-  let createDataLocal = paramLocation.innerText ='Location:' + await weather.name;
+  let createDataLocal = paramLocation.innerText ='Location:' + weather.name;
   let createDataTemp = paramTemp.innerText =
-  'Temp:'+ await weather.main.temp + 'C';
+  'Temp:'+ weather.main.temp + 'C';
   location.append(paramLocation);
   temperature.append(paramTemp);
 }
 
 
+// fetch data regarding city and local temperature
+
 async function getWeatherData(query) {
- 
- let key = 'd6ca49aaa32170856a0c02953f6e0d4f';
- let url =
- "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=metric&appid=" + key + " ";
+ //  authorization key
+ let key = "d6ca49aaa32170856a0c02953f6e0d4f";
+ // api url
+ let url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${key}&units=metric`;
   const res = await fetch(url);
   const data = await res.json();
 
@@ -26,11 +28,7 @@ async function getWeatherData(query) {
     throw new Error(errorMessage);
   } 
   
-   
-
-   
-
-    console.log(  renderWeatherElements(data));
+   console.log(renderWeatherElements(data));
 
 
 }
@@ -41,6 +39,6 @@ getWeatherData().catch((error) => {
 
 
 
-getWeatherData("Stockholm");
+getWeatherData("London");
 
 
