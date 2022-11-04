e062c0530ffd02f4ebd5fcd7a442c577a15afc61
mikaer86 <mikaeleriksson123@gmail.com>
2022-11-04 14:10:05 +0100

    added target values through concatenation

--- js/index.js
+++ js/index.js
@@ -1,21 +1,22 @@
-function renderWeatherElements(weather) {
+async function renderWeatherElements(weather) {
   console.log(weather);
   const location = document.getElementById('location');
   const temperature = document.getElementById('temperature');
   let paramLocation = document.createElement('p');
   let paramTemp = document.createElement('p');
-  let createDataLocal = paramLocation.innerText ='Location:' + weather.name;
+  let createDataLocal = paramLocation.innerText ='Location:' + await weather.name;
   let createDataTemp = paramTemp.innerText =
-  'Temp:' + weather.main.temp + 'C';
+  'Temp:'+ await weather.main.temp + 'C';
   location.append(paramLocation);
   temperature.append(paramTemp);
 }
 
 
 async function getWeatherData(query) {
-  let url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&appid=d6ca49aaa32170856a0c02953f6e0d4f";
-
+ 
+ let key = 'd6ca49aaa32170856a0c02953f6e0d4f';
+ let url =
+ "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=metric&appid=" + key + " ";
   const res = await fetch(url);
   const data = await res.json();
 
@@ -23,12 +24,23 @@ async function getWeatherData(query) {
     const errorMessage = `"Error occured," ${response.status}`;
 
     throw new Error(errorMessage);
-  }
+  } 
+  
+   
+
+   
+
+    console.log(  renderWeatherElements(data));
+
 
 }
 
 getWeatherData().catch((error) => {
   error.errorMessage;
 });
+
+
+
+getWeatherData("Stockholm");
 
 
